bfec180b6dc3b9fdff1c248123c8166507ae0fc3
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-27 19:25:02 -0300

    we can change picker value

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Picker} from 'react-native'
 
-export default props => (
-        <Picker>
-            <Picker.item label="Soma" value="soma" />
-            <Picker.item label="Subtracao" value="subtracao" />
-        </Picker>
-)
+export default class Operacao extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            operacao: ''
+        }
+    }
+    render(){
+        return(
+            <Picker selectedValue={this.state.operacao} 
+                    onValueChange={value => this.setState({operacao: value})}>
+                <Picker.item label="Soma" value="soma" />
+                <Picker.item label="Subtracao" value="subtracao" />
+            </Picker>
+        )
+    }
+}
