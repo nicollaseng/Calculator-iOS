d81db16c9fdedd6abde7588fcbb5f764ca7ce375
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-28 10:47:30 -0300

    button style changed

--- src/components/Comando.js
+++ src/components/Comando.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import {View, Button} from 'react-native'
+import {View, TouchableOpacity, Text,
+        StyleSheet} from 'react-native'
 
 export default props => (
-    <View>
-        <Button title="Calcular" onPress={props.acao}/>
+    <View style={generalView}>
+        <TouchableOpacity onPress={props.acao} style={touchableStyle}>
+            <Text style={textButton}>Calcular</Text>
+        </TouchableOpacity>
     </View>
 )
 
+const styles = StyleSheet.create({
+    generalView:{
+        alignItems: 'center'
+    },
+    textButton: {
+        fontSize: 25,
+        color: 'white',
+        textAlign: 'center',
+        padding: 20
+
+    },
+    touchableStyle: {
+        backgroundColor: '#3399ff',
+        width: 200
+    }
+})
+
+const {generalView, textButton, touchableStyle} = styles
